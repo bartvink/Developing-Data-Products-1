dad628fb4dd0e94bbaf61dda160566e61b4eff6b
KrupaR <Krupa.Rajendran@gmail.com>
2016-06-06 00:57:19 +0530

    Changed
    Minor

---
+++ server.R
@@ -0,0 +1,10 @@
+library(shiny)
+source("getWeight.R")
+
+
+shinyServer(
+  function(input,output) {
+    #weight <- avgWeight(input$height)
+    output$weightLbs <- renderPrint({avgWeight(input$height)})
+    output$weightKgs <- renderPrint({avgWeight(input$height)/2.2})
+  })
---
+++ ui.R
@@ -0,0 +1,18 @@
+
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Predicting average weight of women aged between 30-39 years based on height, using Women dataset of R"),
+  sidebarPanel(
+    numericInput('height','Enter Height in Inches (between 60 and 72)',60,min=60,max=72,step=1),
+    submitButton('Submit'),
+    
+    
+    h4(' Note: Please Run the application in Full screen mode for better view')
+    ),
+  mainPanel(
+        h3(' Average Weight in Pounds'),
+        verbatimTextOutput("weightLbs"),
+        h3(' Average Weight in Kilograms'),
+        verbatimTextOutput("weightKgs")
+    )
+  ))
